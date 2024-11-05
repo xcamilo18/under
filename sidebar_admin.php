3d18df968022ca497168262f07b63c1956b6a19e
Camilo Arango <Camilomusic1597@gmail.com>
2024-11-04 23:22:07 -0500

    first commit

---
+++ sidebar_admin.php
@@ -0,0 +1,66 @@
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" >
+        <div class="sidebar-brand-icon ">
+            <img src="img/admin.png" alt="" style="width: 35px;">
+        </div>
+        <div class="sidebar-brand-text mx-3">Admin</div>
+    </a>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
+
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item active">
+        <a class="nav-link" href="index_admin.php">
+            <img src="img/casa.png" alt="" style="width: 35px;">
+            <span>Inicio</span></a>
+    </li>
+    
+
+            <!-- Divider -->
+            <hr class="sidebar-divider">
+
+            <!-- Nav Item - Pages Collapse Menu -->
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
+                    aria-expanded="true" aria-controls="collapseTwo">
+                    <img src="img/persona.png" alt="" style="width: 35px;">
+                    <span style="color: black;"><strong>Usuario</strong></span>
+                </a>
+                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <a class="collapse-item" href="add_user.php">Agregar Nuevo</a>
+                        <a class="collapse-item" href="gestion_usuario.php">Gestión de Usuario</a>
+                    </div>
+                </div>
+            </li>
+
+            <!-- Nav Item - Utilities Collapse Menu -->
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
+                    aria-expanded="true" aria-controls="collapseUtilities">
+                    <img src="img/curso.png" alt="" style="width: 35px;">
+                    <span style="color: black;"><strong>Cursos</strong></span>
+                </a>
+                <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
+                    data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <a class="collapse-item" href="create_curso.php">Agregar Nuevo</a>
+                        <a class="collapse-item" href="gestion_cursos.php">Gestión de Cursos</a>
+                    </div>
+                </div>
+            </li>
+            
+            <!-- Divider -->
+            <hr class="sidebar-divider d-none d-md-block">
+
+            <!-- Sidebar Toggler (Sidebar) -->
+            <div class="text-center d-none d-md-inline">
+                <button class="rounded-circle border-0" id="sidebarToggle"></button>
+            </div>
+
+            <!-- Sidebar Message -->
+            <!--  -->
+
+        </ul>
---
+++ sidebar_student.php
@@ -0,0 +1,61 @@
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" >
+        <div class="sidebar-brand-icon ">
+            <img src="img/estudiante.png" alt="" style="width: 35px;">
+        </div>
+        <div class="sidebar-brand-text mx-3">Estudiante</div>
+    </a>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
+
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item active">
+        <a class="nav-link" href="index_student.php">
+            <img src="img/casa.png" alt="" style="width: 35px;">
+            <span>Inicio</span></a>
+    </li>
+            <!-- Divider -->
+            <hr class="sidebar-divider">
+
+            <!-- Nav Item - Pages Collapse Menu -->
+            
+
+            <!-- Nav Item - Utilities Collapse Menu -->
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
+                    aria-expanded="true" aria-controls="collapseUtilities">
+                    <img src="img/curso.png" alt="" style="width: 35px;">
+                    <span style="color: black;"><strong>Cursos</strong></span>
+                </a>
+                <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
+                    data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <a class="collapse-item" href="show_cursos.php">Matriculados</a>
+                        <h6 class="collapse-header">TODOS LOS CURSOS: </h6>
+                        <a class="collapse-item" href="calculo.php">Cálculo</a>
+                        <a class="collapse-item" href="programacion.php">Programación</a>
+                        <a class="collapse-item" href="fisica.php">Física</a>
+                    </div>
+                </div>
+            </li> 
+
+            
+
+            <!-- Nav Item - Pages Collapse Menu -->
+            
+
+            
+            <!-- Divider -->
+            <hr class="sidebar-divider d-none d-md-block">
+
+            <!-- Sidebar Toggler (Sidebar) -->
+            <div class="text-center d-none d-md-inline">
+                <button class="rounded-circle border-0" id="sidebarToggle"></button>
+            </div>
+
+            <!-- Sidebar Message -->
+            <!--  -->
+
+        </ul>
